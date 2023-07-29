705595c7333b055abf8743e220add13e2cb8cc68
Gael Hofman <Gieltie@outlook.fr>
2023-07-29 14:38:31 +0200

    netoyage pour demarage react app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "./App.scss";
-function App() {
+
+export default function App() {
   return (
-    <div className="wrapper">
-      <h1>Test des boutons react app</h1>
-      <header className="wrapper__btns">
-        <button className="bleu">Bouton Bleu</button>
-        <button className="red">Bouton Rouge</button>
-        <button className="green">Bouton Vert</button>
-      </header>
+    <div>
+      <h1>Kasa</h1>
     </div>
   );
 }
-export default App;
