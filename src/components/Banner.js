f525145496416f0dd8f52f6f60cd553603ec77a8
Gael Hofman <Gieltie@outlook.fr>
2023-08-01 22:35:40 +0200

    header, banner et footer en desktop ok

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Banner() {
   return (
     <section className="banner">
-      <h2>yeah man yoyoyoyoy</h2>
+      <h1 className="banner--text">Chez vous, partout et ailleurs</h1>
     </section>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Main() {
   return (
     <main className="main">
-      <h3>yeah man yoyoyoyoy</h3>
+      <h3>Main section</h3>
     </main>
   );
 }
