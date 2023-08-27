03306dde1165da322547ef558b315fe46e328d8e
Gael Hofman <Gieltie@outlook.fr>
2023-08-27 18:44:55 +0200

    site terminee, reste a verifier si tout ok

--- src/components/SheetInfo.jsx
+++ src/components/SheetInfo.jsx
@@ -1,24 +1,24 @@
 export default function SheetInfo(props) {
     return (
-        <div className='description'>
-            <div className='description__title'>
-                <h1>{props.title}</h1>
-                <p>{props.location}</p>
-                <ul>
+        <div className="description">
+            <div className="description__info">
+                <h1 className="description__info--title">{props.title}</h1>
+                <p className="description__info--location">{props.location}</p>
+                <ul className="description__info--tags">
                     {props.tags.map((tag, index) => (
                         <li key={index}>{tag}</li>
                     ))}
                 </ul>
             </div>
 
-            <div className='description__host'>
-                <div className='description__host--img'>
+            <div className="description__host">
+                <div className="description__host--info">
                     <p>{props.name}</p>
                     <img src={props.picture} alt={props.name} />
                 </div>
-                <ul>
+                <ul className="description__host--stars">
                     {[1, 2, 3, 4, 5].map((number, index) => (
-                        <li key={index} className={props.rating >= number ? "full" : ""}><i className="fa-solid fa-star"></i></li>
+                        <li key={index} className={props.rating >= number ? "star-filled" : ""}><i className="fa-solid fa-star"></i></li>
                         ))}
                 </ul>
             </div>
